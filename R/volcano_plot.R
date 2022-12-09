27f9f97086dcc7d3a1f710276188cb2374f72c22
LuisCoy <luiscoy@hotmail.co.uk>
2022-12-09 15:00:44 +0000

    fixed pvalue bug

--- R/volcano_plot.R
+++ R/volcano_plot.R
@@ -10,23 +10,24 @@
 #' @export
 #'
 #' @examples volcano_plot(example_proteomic_data)
-volcano_plot <- function(df, x = "Log2fold", y = "Log10pvalue", p_value = 0.05, Log2foldvalue = 1){
+volcano_plot <- function(df, x = "Log2fold", y = "Log10pvalue", p_value = 0.05, log2foldvalue = 1, point_size = 2, text_size = 20){
   x_axis_length <- base::round(max(sqrt(df[,x]^2)), 0)+1
 
-    df_regulation <- dplyr::mutate(df, "regulation" = dplyr::case_when(df[,x] >= 1 & df[,y] <= log10(p_value) ~ "up",
-                                                                       df[,x] <= -1 & df[,y] <= log10(p_value) ~ "down",
+    df_regulation <- dplyr::mutate(df, "regulation" = dplyr::case_when(df[,x] >= log2foldvalue & df[,y] >= -log10(p_value) ~ "up",
+                                                                       df[,x] <= -log2foldvalue & df[,y] >= -log10(p_value) ~ "down",
                                                                        T ~ "neutral"))
 
     ggplot2::ggplot(data = df_regulation,ggplot2::aes_string(x = x, y = y, col = "regulation"))+
-    ggplot2::geom_point()+
+    ggplot2::geom_point(size = point_size)+
     ggplot2::labs(x = base::expression("Log"[2]* " (Fold Change)"),
          y = expression("-Log"[10]* " (p-value)"),
          tag = NULL,
          title = NULL)+
-    ggplot2::geom_vline(xintercept = c(-Log2foldvalue,Log2foldvalue), color = "black", size = 1, linetype = "dashed")+
+    ggplot2::geom_vline(xintercept = c(-log2foldvalue,log2foldvalue), color = "black", size = 1, linetype = "dashed")+
     ggplot2::geom_hline(yintercept = -log10(p_value), color = "black", size = 1, linetype = "dashed")+
     ggplot2::scale_x_continuous(breaks = seq(-x_axis_length,x_axis_length,1),
                        limits = c(-x_axis_length,x_axis_length))+
-    ggplot2::scale_colour_manual(values = c("up" = "#F60239", "down" = "#00E307", "neutral" = "#000000"),
-                                                                                     labels = c("", "", ""))
+    ggplot2::scale_colour_manual(values = c("up" = "#F60239", "down" = "#00E307", "neutral" = "#000000"),labels = c("", "", ""))+
+    ggplot2::theme_light()+
+    ggplot2::theme(legend.position = "none", text = ggplot2::element_text(size = text_size))
 }
