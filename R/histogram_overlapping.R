9df1fb183da0b29797247f6421bcedd69427f227
LuisCoy <luiscoy@hotmail.co.uk>
2023-02-20 22:58:55 +0000

    Modified arguments: labels and binwidth

--- R/histogram_overlapping.R
+++ R/histogram_overlapping.R
@@ -1,25 +1,32 @@
-#' Histogram Overlapping
+#' Plot two-group histograms
 #'
-#' @param df
-#' @param x
-#' @param a
-#' @param b
-#' @param group
-#' @param a_colour
-#' @param b_colour
-#' @param bins
-#' @param x_label
-#' @param y_label
-#' @param title
-#' @param tag
-#' @param legend_label
-#' @param alpha
+#' This function creates a histogram plot of two groups with overlapping histograms
+#' and adds vertical lines to indicate the median value of each group.
 #'
-#' @return Histogram
-#' @export
+#' @param df Dataframe containing the data to plot
+#' @param x Character string giving the column containing the values
+#' @param a Character string giving the name of group a e.g. "Treated"
+#' @param b Character string giving the name of group b e.g. "Control"
+#' @param group Character string giving the column containing the group IDs
+#' @param colour.a Character string giving the fill color for group a; defaults to "#F748A5"
+#' @param colour.b Character string giving the fill color for group b;defaults to "#3BB9FF"
+#' @param bins Number of bins to use in the histogram; defaults to 100
+#' @param binwidth Width of the bins in the histogram; defaults to NULL
+#' @param label.x X axis label; defaults to NULL
+#' @param label.y Y axis label; defaults to "Count"
+#' @param title Character string giving the plot title; defaults to NULL
+#' @param tag Character string giving the plot tag; defaults to NULL
+#' @param label.legend Character string giving the legend label; defaults to NULL
+#' @param alpha The transparency of the fill color; defaults to 0.5
+#'
+#' @return A ggplot2 histogram plot with vertical lines indicating the median value of each group
 #'
 #' @examples
-histogram_overlapping <- function(df, x, a, b, group, a_colour = "#F748A5", b_colour = "#3BB9FF", bins = 100, x_label = NULL, y_label = "Count", title = NULL, tag = NULL, legend_label = NULL, alpha = 0.5){
+#' df <- data.frame(abundances = rnorm(1000), group = sample(c("Treated", "Control"), 1000, replace = TRUE))
+#' histogram_overlapping(df, "abundances", "Treated", "Control", "group", label.x = "Protein Abundances", label.legend = "Groups")
+#'
+#' @export
+histogram_overlapping <- function(df, x, a, b, group, colour.a = "#F748A5", colour.b = "#3BB9FF", bins = 100, binwidth = NULL, label.x = NULL, label.y = "Count", title = NULL, tag = NULL, label.legend = NULL, alpha = 0.5){
 
   #Convert to dataframe to allow median to be calculated
   dataframe <- base::data.frame(df)
@@ -30,14 +37,14 @@ histogram_overlapping <- function(df, x, a, b, group, a_colour = "#F748A5", b_co
   median_b <- stats::median(dataframe[dataframe[,group] == b, x])
 
   ggplot2::ggplot(data = dataframe)+
-    ggplot2::geom_histogram(ggplot2::aes_string(x = x, fill = group), position = "identity", colour = "white", alpha = alpha, bins = bins)+
-    ggplot2::geom_vline(xintercept = median_a, col = a_colour, lwd = 0.5, alpha = alpha)+
-    ggplot2::geom_vline(xintercept = median_b, col = b_colour, lwd = 0.5, alpha = alpha)+
-    ggplot2::scale_fill_manual(name = legend_label,
-                               values = c(a_colour,b_colour),
+    ggplot2::geom_histogram(ggplot2::aes_string(x = x, fill = group), position = "identity", colour = "white", alpha = alpha, bins = bins, binwidth = binwidth)+
+    ggplot2::geom_vline(xintercept = median_a, col = colour.a, lwd = 0.5, alpha = alpha)+
+    ggplot2::geom_vline(xintercept = median_b, col = colour.b, lwd = 0.5, alpha = alpha)+
+    ggplot2::scale_fill_manual(name = label.legend,
+                               values = c(colour.a, colour.b),
                                labels = c(a,b))+
-    ggplot2::labs(x = base::bquote(Log[2]*~(.(x_label))),
-                  y = y_label,
+    ggplot2::labs(x = label.x,
+                  y = label.y,
                   title = title,
                   tag = tag)+
     ggplot2::theme_light()
