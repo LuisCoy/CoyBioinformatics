21d09b5a40a0e6abc3bd5a87d220bb088084b77a
Luis Coy <luiscoy@hotmail.co.uk>
2023-10-13 17:47:35 +0100

    Legend options

--- R/missing_data_plot.R
+++ R/missing_data_plot.R
@@ -9,9 +9,13 @@
 #' @param colours A character vector giving the colours; defaults to "#F748A5" and "#3DB7E9".
 #' @param x_label X axis label; defaults to "Samples"
 #' @param y_label Y axis label; defaults to "Number of proteins"
+#' @param text_size Integer value specifying the text size in the plot; defaults to 20.
 #' @param title A character string giving the plot Title
 #' @param tag A character string giving the  plot tag
 #' @param legend_label A character string giving the legend label
+#' @param legend_position A vector for the legend position; defaults to c(0.8, 0.2).
+#' @param legend_colour Hexadecimal color code for the legend background; defaults to white.
+#' @param legend_alpha A value between 0 and 1 for the transparency of the legend; defaults to 0.5.
 #' @param transparent A logical value for a transparent background; defaults to True.
 #'
 #' @return plot
@@ -27,9 +31,13 @@ missing_data_plot <-
            colours = c("#F748A5","#3DB7E9"),
            x_label = "Samples",
            y_label = "Number of proteins",
+           text_size = 20,
            title = NULL,
            tag = NULL,
            legend_label = NULL,
+           legend_position = c(0.6,0.8),
+           legend_colour = "white",
+           legend_alpha = 0.5,
            transparent = T) {
     #plot theme
     if (transparent) {
@@ -60,5 +68,9 @@ missing_data_plot <-
         fill = legend_label
       ) +
       ggplot2::theme_light()+
+      ggplot2::theme(legend.background = ggplot2::element_rect(fill = scales::alpha(legend_colour,legend_alpha), colour = NULL),
+                     legend.position = legend_position,
+                     legend.key = ggplot2::element_rect(fill = scales::alpha("white", 0)),
+                     text = ggplot2::element_text(size = text_size)) +
       plot_theme
   }
