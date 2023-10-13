a77fd55c9488f6d050ffd644347a8b151b207689
Luis Coy <luiscoy@hotmail.co.uk>
2023-10-13 17:32:49 +0100

    plot colour options added

--- R/missing_data_plot.R
+++ R/missing_data_plot.R
@@ -6,26 +6,59 @@
 #' @param name A character string giving the column containing present or missing
 #' @param value A character string giving the column containing the number of proteins present or missing
 #' @param group A character string giving the column with the group IDs e.g. "Treated" and "Control"
+#' @param colours A character vector giving the colours; defaults to "#3DB7E9" and "#F748A5".
 #' @param x_label X axis label; defaults to "Samples"
 #' @param y_label Y axis label; defaults to "Number of proteins"
 #' @param title A character string giving the plot Title
 #' @param tag A character string giving the  plot tag
 #' @param legend_label A character string giving the legend label
+#' @param transparent A logical value for a transparent background; defaults to True.
 #'
 #' @return plot
 #' @export
 #'
 #' @examples
-missing_data_plot <- function(df, sample, name, value, group, x_label = "Samples", y_label = "Number of proteins", title = NULL, tag = NULL, legend_label = NULL){
-
-  ggplot2::ggplot(data = df, ggplot2::aes_string(x = sample, y = value, fill = name))+
-    ggplot2::geom_col()+
-    ggplot2::facet_grid(~.data[[group]], scales = "free", space = "free")+
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))+
-    ggplot2::labs(x = x_label,
-                  y = y_label,
-                  title = title,
-                  tag = tag,
-                  fill = legend_label)+
-    ggplot2::theme_light()
-}
+missing_data_plot <-
+  function(df,
+           sample,
+           name,
+           value,
+           group,
+           colours = c("#3DB7E9","#F748A5"),
+           x_label = "Samples",
+           y_label = "Number of proteins",
+           title = NULL,
+           tag = NULL,
+           legend_label = NULL,
+           transparent = T) {
+    #plot theme
+    if (transparent) {
+      plot_theme <- ggplot2::theme(
+        panel.background = ggplot2::element_rect(fill = 'transparent'),
+        plot.background = ggplot2::element_rect(fill = 'transparent', color =
+                                                  NA)
+      )
+    } else {
+      plot_theme <-
+        ggplot2::theme()
+    }
+    # plot
+    ggplot2::ggplot(data = df, ggplot2::aes_string(x = sample, y = value, fill = name)) +
+      ggplot2::geom_col() +
+      ggplot2::facet_grid( ~ .data[[group]], scales = "free", space = "free") +
+      ggplot2::scale_fill_manual(values = colours)+
+      ggplot2::theme(axis.text.x = ggplot2::element_text(
+        angle = 90,
+        vjust = 0.5,
+        hjust = 1
+      )) +
+      ggplot2::labs(
+        x = x_label,
+        y = y_label,
+        title = title,
+        tag = tag,
+        fill = legend_label
+      ) +
+      ggplot2::theme_light()+
+      plot_theme
+  }
