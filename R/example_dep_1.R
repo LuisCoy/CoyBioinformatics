37e6cd79076f21d34076f28493464a542f9cfcc0
LuisCoy <luiscoy@hotmail.co.uk>
2024-05-28 20:39:56 +0100

    example proteomic DEP data

---
+++ R/example_dep_1.R
@@ -0,0 +1,6 @@
+#' Example proteomic differential expression data
+#'
+#' @source DEP package label free quantification proteomic data called UbiLength and originating from Zhang et al. 2017. Differential expression analysis was performed comparing condition Ubiquitin 4 and control
+#'
+#' @examples data(example_dep_1)
+"example_dep_1"
